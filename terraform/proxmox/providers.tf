920fcb67c0e4e829f89adc94c4ba3939f827fa49
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2024-07-18 13:29:55 -0400

    Update providers.tf

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -11,7 +11,7 @@ terraform {
 provider "proxmox" {
   pm_tls_insecure     = true
   pm_api_url          = "https://192.168.1.201:8006/api2/json"
-  pm_api_token_secret = "cc2e72b9-d176-4fe7-8a72-80bdd89c7adc"
+  pm_api_token_secret = "************************************"
   pm_api_token_id     = "root@pam!terraform"
 }
-#cc2e72b9-d176-4fe7-8a72-80bdd89c7adc
+#
